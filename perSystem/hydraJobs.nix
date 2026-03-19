be6e9b6bb78b3f473f56540de231f7e880e57d53
John Lotoski <john.lotoski@iohk.io>
2026-03-19 17:52:34 -0500

    ci: add an aggregate required of drvs, checks, devShells

--- perSystem/hydraJobs.nix
+++ perSystem/hydraJobs.nix
@@ -1,21 +1,18 @@
-{ inputs, config, withSystem, lib, ... }: {
-  perSystem = { hsPkgs, pkgs, ... }:
-    let
-      td = hsPkgs.trace-dispatcher;
-    in
+{ config, lib, withSystem, ... }: {
+  flake.hydraJobs = lib.genAttrs config.systems (lib.flip withSystem (
     {
-      legacyPackages.hydraJobs =
-        pkgs.callPackages inputs.iohkNix.utils.ciJobsAggregates
-          {
-            ciJobs = {
-              "trace-dispatcher:lib:trace-dispatcher" = td.components.library;
-              "trace-dispatcher:test:trace-dispatcher-test" = td.components.tests.trace-dispatcher-test;
-              revision = pkgs.writeText "revision" (inputs.self.rev or "dirty");
-            };
-          };
-    };
-
-  flake.hydraJobs = lib.genAttrs config.systems (system:
-    withSystem system ({ config, ... }: config.legacyPackages.hydraJobs)
-  );
+      config,
+      pkgs,
+      ...
+    }: let
+      jobs = {inherit (config) packages checks devShells;};
+    in
+      jobs
+      // {
+        required = pkgs.releaseTools.aggregate {
+          name = "required";
+          constituents = lib.collect lib.isDerivation jobs;
+        };
+      }
+  ));
 }
