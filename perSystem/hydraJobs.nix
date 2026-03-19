fbc8c681123629cc412acbf28e727c3745d97d3c
John Lotoski <john.lotoski@iohk.io>
2026-03-19 18:06:20 -0500

    ci: make explicit required and nonrequired jobs

--- perSystem/hydraJobs.nix
+++ perSystem/hydraJobs.nix
@@ -5,13 +5,19 @@
       pkgs,
       ...
     }: let
-      jobs = {inherit (config) packages checks devShells;};
+      required    = {inherit (config) packages checks;};
+      nonRequired = {inherit (config) devShells;};
+      jobs        = required // nonRequired;
     in
       jobs
       // {
         required = pkgs.releaseTools.aggregate {
           name = "required";
-          constituents = lib.collect lib.isDerivation jobs;
+          constituents = lib.collect lib.isDerivation required;
+        };
+        nonrequired = pkgs.releaseTools.aggregate {
+          name = "nonrequired";
+          constituents = lib.collect lib.isDerivation nonRequired;
         };
       }
   ));
