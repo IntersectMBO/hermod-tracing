33aa85def840445d4aa86ad4f2212c3e4c8b7cef
Michael Karg <michael.karg@iohk.io>
2026-06-09 12:04:53 +0200

    nix: hermod-recon-framework flake outputs

--- perSystem/packages.nix
+++ perSystem/packages.nix
@@ -1,10 +1,16 @@
 {
   perSystem = { hsPkgs, ... }:
     let
-      td = hsPkgs.trace-dispatcher;
+      td  = hsPkgs.trace-dispatcher;
+      hrf = hsPkgs.hermod-recon-framework;
     in
     {
-      packages.trace-dispatcher = td.components.library;
-      checks.trace-dispatcher-test = td.components.tests.trace-dispatcher-test;
+      packages.trace-dispatcher          = td.components.library;
+      checks.trace-dispatcher-test       = td.components.tests.trace-dispatcher-test;
+
+      packages.hermod-recon              = hrf.components.exes.hermod-recon;
+      packages.hermod-recon-grep         = hrf.components.exes.hermod-recon-grep;
+      checks.hermod-recon-test           = hrf.components.tests.hermod-recon-test;
+      checks.hermod-recon-integration-test = hrf.components.tests.hermod-recon-integration-test;
     };
 }
